906b0121579c241364e9efa5a424f9aeaffbe64d
Rohit Pandey <croods660@gmail.com>
2024-04-03 15:37:55 +0530

    I Completed Day 11

---
+++ bye.py
@@ -0,0 +1,5 @@
+
+
+from hello import hello
+
+hello("Python 3.8.2")
---
+++ hello.py
@@ -0,0 +1,12 @@
+
+
+print("I am still Learning Python! ")
+
+def hello(n):
+    print("Hello, " + n)
+
+hello("Python")
+
+hello_one = "Hello, Python!"
+hello_two = "Hello, JavaScript!"
+hello_three = "Hello, Java!"
